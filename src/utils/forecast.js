3dda8f54f1b6db011fb0beb84ca5a37c7a9203df
Archi jain <archijain378@gmail.com>
2021-02-02 14:33:28 +0530

    About text and Temp in celsius

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const request = require('request')
 //    - Success, pass forecast string for data (same format as from before)
 
 const forecast =(latitude, longitude, callback) =>{
-    const url = 'http://api.weatherstack.com/current?access_key=71fda936e7c2d4f51a0991ecbebd0e91&query='+latitude+','+longitude+'&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=71fda936e7c2d4f51a0991ecbebd0e91&query='+latitude+','+longitude+'&units=m'
     request ({url, json:true}, (error, {body})=>{
         if(error){
             callback('Unable to connect to weather services')
