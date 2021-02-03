c62739087510989bb6638a53a547281a984bd9fe
Archi jain <archijain378@gmail.com>
2021-02-03 15:10:00 +0530

    Final changes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,14 +1,4 @@
 const request = require('request')
-
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
-
 const forecast =(latitude, longitude, callback) =>{
     const url = 'http://api.weatherstack.com/current?access_key=71fda936e7c2d4f51a0991ecbebd0e91&query='+latitude+','+longitude+'&units=m'
     request ({url, json:true}, (error, {body})=>{
